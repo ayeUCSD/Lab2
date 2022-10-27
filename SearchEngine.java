a79ae28fca28e2d04efe0692e425456a2c94303f
ayeUCSD <114363008+ayeUCSD@users.noreply.github.com>
2022-10-27 15:19:17 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -15,63 +15,56 @@ class SearchEngine {
         Server.start(port, new Handler());
     }
 
-    }
+}
 
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
     ArrayList<String> list = new ArrayList<String>();
 
-    class Handler implements URLHandler {
-        // The one bit of state on the server: a number that will be manipulated by
-        // various requests.
-
-        public String handleRequest(URI url) {
-            // checks if path is "empty"
-            if (url.getPath().equals("/")) {
-                return String.format("Here is whats in the List!:\n" + String.join(", ", list));
-            } else if (url.getPath().equals("/addFiller")) {
-                list.add("filler");
-                return String.format("it added the word \"filler\"!");
-            } else {
-                System.out.println("Path: " + url.getPath());
-                String[] parameters = url.getQuery().split("=");
-                // Code for Adding
-                if (url.getPath().contains("/add")) {
-                    return add(parameters);
-                }
-
-                else if (url.getPath().contains("/query")) {
-                    // Code for Searching
-                    return search(parameters);
-                }
+    public String handleRequest(URI url) {
+        // checks if path is "empty"
+        if (url.getPath().equals("/")) {
+            return String.format("Here is whats in the List!:\n" + String.join(", ", list));
+        } else if (url.getPath().equals("/addFiller")) {
+            list.add("filler");
+            return String.format("it added the word \"filler\"!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            String[] parameters = url.getQuery().split("=");
+            // Code for Adding
+            if (url.getPath().contains("/add")) {
+                return add(parameters);
             }
 
-            return "bruh wtf error code 404 up in here\n you fucked up lol";
+            else if (url.getPath().contains("/query")) {
+                // Code for Searching
+                return search(parameters);
+            }
         }
+
+        return "bruh wtf error code 404 up in here\n you fucked up lol";
     }
 
     public String add(String[] parameters) {
         // if we in fact are adding.... check for the "a"
-        if (parameters[0].equals("a")) {
             list.add(parameters[1]);
             return String.format("Added " + parameters[1] + " to the List!");
-        }
     }
 
-public String search(String[] parameters){
-    // if we in fact are searching.... check for the "s"
-    if (parameters[0].equals("s")) {
+    public String search(String[] parameters) {
+        // if we in fact are searching.... check for the "s"
         ArrayList<String> tempList = new ArrayList<String>();
-        Iterator iter = list.iterator();
-        while (iter.hasNext()) {
-            String temp = (String) iter.next();
-            if (temp.contains(parameters[1])) {
-                tempList.add(temp);
+        if (parameters[0].equals("s")) {
+            Iterator iter = list.iterator();
+            while (iter.hasNext()) {
+                String temp = (String) iter.next();
+                if (temp.contains(parameters[1])) {
+                    tempList.add(temp);
+                }
             }
         }
-
         return String.format("Result(s): " + String.join(", ", tempList));
-}
-
-
-
+    }
 
 }
